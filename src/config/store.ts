13ec27dd070a40895099ec2f4007e4c5c4ed439d
Jyotishman Pathak <jyotishmanpathak25@gmail.com>
2026-04-28 13:17:15 +0530

    Change contact email and update address information
    Updated contact email and address details.

--- src/config/store.ts
+++ src/config/store.ts
@@ -7,10 +7,10 @@ export const STORE_CONFIG = {
   phone: "+91 9502502440",
   // The WhatsApp number should be without '+' or spaces, just the country code and number
   whatsappNumber: "919502502440", 
-  email: "rkcomputers@gmail.com",
+  email: "rkcomputersnrt@gmail.com",
   
   // --- LOCATION ---
-  address: "near Murali krishna Resturant, Arundelpet, Narasaraopeta, Andhra Pradesh 522601",
+  address: " Arundelpet near Tamilnadu Bank beside jeevan bakery Narasaraopeta, Andhra Pradesh 522601",
   shortAddress: "Andhra Pradesh",
   // Go to Google Maps -> share -> Embed a map -> copy the URL inside the src="..." attribute
   mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3581.42224486021!2d91.7843559!3d26.1503746!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a59d46cc2adb5%3A0x7aeffa254fa6b151!2sDYNATEK!5e0!3m2!1sen!2sin!4v1777042819940!5m2!1sen!2sin",
